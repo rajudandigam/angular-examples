4c93916ba601388ac203d4f2e616b92cb741c252
rajudandigam <dandigam.raju@gmail.com>
2025-03-19 12:34:21 -0700

    Added seperate route for date pipe example

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { DatePipeExampleComponent } from '../app/components/date-pipe-example/date-pipe-example.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'datepipe-example',
+        component: DatePipeExampleComponent
+    }
+];
