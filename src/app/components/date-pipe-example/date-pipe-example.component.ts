70ab901b7efc4371464f5d491869e38aa85d67f7
rajudandigam <dandigam.raju@gmail.com>
2025-03-19 00:27:14 -0700

    Added few more options

--- src/app/components/date-pipe-example/date-pipe-example.component.ts
+++ src/app/components/date-pipe-example/date-pipe-example.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-date-pipe-example',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './date-pipe-example.component.html',
   styleUrl: './date-pipe-example.component.scss'
 })
 export class DatePipeExampleComponent {
-
+  currentDate = new Date();
 }
